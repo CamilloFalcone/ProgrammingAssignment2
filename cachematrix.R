c1cd6261f189078f9e97adfc5d1a565517ad3994
Camillo Falcone <camillo.falcone@gmail>
2016-05-13 07:08:34 +0200

    Yet another commit..

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,7 @@
 
 ## Write a short comment describing this function
 ## this function provides the basic methods to manage a cached matrix, namely set and get
-## and two methods to set and get the inverse version of it
+## and two methods to set and get the inverse version 
 
 makeCacheMatrix <- function(x = matrix()) {
 	m <- NULL
@@ -21,8 +21,22 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function receives as a parameter a squared CacheMatrix (as obtained with the makeCacheMatrix) 
+## and return its inverse by 
+## invoking the getinverse method, 
+## checking for the availability of a cached value
+## if a cached value doesn't exist, it provides to evaluate the inverse matrix by using the solve R function
+## the value obtained is then cached (using the setinverse method) for future calls.. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
